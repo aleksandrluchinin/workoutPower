2fe3f1dcbd78095f80d94912e665fe3d4dbe2aaf
aleksandrluchinin <aleksandr-luchinin@bk.ru>
2022-12-09 16:58:39 +0500

    Initial Commit

---
+++ workoutPower/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  workoutPower
+//
+//  Created by Aleksandr  on 09.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        view.backgroundColor = .blue
+    }
+
+
+}
+
